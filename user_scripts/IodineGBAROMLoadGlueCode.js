a61514132caf4943ea18b16b0873e1eff49f5a28
MK Games <37133977+MkGamesdev@users.noreply.github.com>
2019-11-21 17:41:18 +1100

    Update IodineGBAROMLoadGlueCode.js

--- user_scripts/IodineGBAROMLoadGlueCode.js
+++ user_scripts/IodineGBAROMLoadGlueCode.js
@@ -10,7 +10,7 @@
  */
 if ( window.location !== window.parent.location ) {
     console.log('MKGBA error: Ask for your site to be whitelisted at https://discord.gg/g7qRZW6')
- alert('MKGBA error: Ask for your site to be whitelisted at https://discord.gg/g7qRZW6');
+ //alert('MKGBA error: Ask for your site to be whitelisted at https://discord.gg/g7qRZW6');
    location.href='https://mkgamesdev.github.io/MKGBA2.0/permission.html ';
 throw error;
 }
